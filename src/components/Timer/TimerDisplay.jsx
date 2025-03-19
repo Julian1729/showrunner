5200f2472b0120cbf824a32593c1995f1c9330df
Julian Hernandez <julian1729@Julians-Macbook-Pro-M1-Max-2.local>
2025-03-19 16:35:28 -0400

    add hours to main timer

--- src/components/Timer/TimerDisplay.jsx
+++ src/components/Timer/TimerDisplay.jsx
@@ -1,11 +1,11 @@
 import { useTimerContext } from "../../contexts/TimerContext";
 
 export default function TimerDisplay() {
-  const { seconds, minutes } = useTimerContext();
+  const { hours, seconds, minutes } = useTimerContext();
 
   return (
     <div style={{ fontSize: "100px" }}>
-      <span>{minutes}</span>:
+      <span>{hours}</span>:<span>{minutes}</span>:
       <span>{seconds <= 9 ? `0${seconds}` : seconds}</span>
     </div>
   );
